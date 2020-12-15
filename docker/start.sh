a20cb8c4d16d79778a2f946041c4b99dbd88d534
Sitoi <133397418@qq.com>
2020-12-15 13:17:45 +0800

    修改 dockerfile 文件

--- docker/start.sh
+++ docker/start.sh
@@ -3,15 +3,16 @@ set -e
 
 export LANG="zh_CN.UTF-8"
 
-echo "定时任务更新代码，git 拉取最新代码，并安装更新依赖..."
-git -C /dailycheckin pull
+echo "Git 拉取最新代码，并安装更新依赖..."
+git clone https://github.com/Sitoi/dailycheckin.git /dailycheckin
+mkdir -p /dailycheckin/logs
 pip install -r /dailycheckin/requirements.txt
-
-sed -i 's/>>/|ts >>/g' $CRONTAB_LIST_FILE
-crontab $CRONTAB_LIST_FILE
 
 echo "Load the latest crontab task file..."
 echo "加载最新的定时任务文件..."
+
+sed -i 's/>>/|ts >>/g' $CRONTAB_LIST_FILE
+crontab $CRONTAB_LIST_FILE
 
 echo "Start crontab task main process..."
 echo "启动crondtab定时任务主进程..."
