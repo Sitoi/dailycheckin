f10f9b549ddc3d5d9a6967ee9bb0827b4d86b82c
shitao <133397418@qq.com>
2021-02-24 00:53:08 +0800

    feat: 优化一件启动 docker 脚本，添加环境检测。

--- docker_start.sh
+++ docker_start.sh
@@ -1,15 +1,34 @@
 #!/bin/bash
 
-curl -O https://raw.githubusercontent.com/Sitoi/dailycheckin/main/docker/docker-compose.yml
-
-curl -O https://raw.githubusercontent.com/Sitoi/dailycheckin/main/docker/Makefile
+echo "在当前目录下创建 config 文件夹"
 
 mkdir -p config
 
+echo "下载渲染 config 文件的脚本，并执行渲染"
+
 curl https://raw.githubusercontent.com/Sitoi/dailycheckin/main/deploy.sh | bash
 
-docker run -d -v $(pwd)/config:/dailycheckin/config \
-  -v $(pwd)/logs:/dailycheckin/logs \
-  --name dailycheckin \
-  --restart always \
-  sitoi/dailycheckin:latest
+docker --version
+if [ $? -ne 0 ];then
+  echo "未安装 docker ，请先安装 docker 再运行脚本。"
+else
+  echo "docker 环境存在，检测 docker-compose 环境是否安装..."
+  docker-compose --version
+  if [ $? -ne 0 ];then
+    echo "未安装 docker-compose，将使用 docker 命令启动容器..."
+    echo "开始通过 docker 命令创建容器"
+    docker run -d -v $(pwd)/config:/dailycheckin/config \
+      -v $(pwd)/logs:/dailycheckin/logs \
+      --name dailycheckin \
+      --restart always \
+      sitoi/dailycheckin:latest
+  else
+    echo "docker-compose 环境存在，将使用 docker-compose 命令启动容器..."
+    echo "下载 docker-compose.yml 文件"
+    curl -O https://raw.githubusercontent.com/Sitoi/dailycheckin/main/docker/docker-compose.yml
+    echo "下载 Makefile 文件（可以无视）"
+    curl -O https://raw.githubusercontent.com/Sitoi/dailycheckin/main/docker/Makefile
+    echo "开始通过 docker-compose 命令创建容器"
+    docker-compose up -d
+  fi
+fi
