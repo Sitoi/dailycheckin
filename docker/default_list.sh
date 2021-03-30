b2f023f3e7acbbf2d64c2980c4bfa4623242a50c
Sitoi <133397418@qq.com>
2021-03-30 11:49:49 +0800

    :hammer: 添加【AcFun】点赞、弹幕、投币任务； :hammer: 修改默认定时任务执行时间，减少 clone 次数

--- docker/default_list.sh
+++ docker/default_list.sh
@@ -1,7 +1,7 @@
 #必须要的默认定时任务请勿删除
-52 */1 * * * sh /dailycheckin/docker/default_task.sh >> /dailycheckin/logs/default_task.log 2>&1
+0 */12 * * * sh /dailycheckin/docker/default_task.sh >> /dailycheckin/logs/default_task.log 2>&1
 # 每天的 23:50 分清理一次日志
-50 23 */1 * * rm -rf /dailycheckin/logs/*.log
+50 23 */2 * * rm -rf /dailycheckin/logs/*.log
 
 
 ##############每日签到一次任务##############
