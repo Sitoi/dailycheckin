aaeb91474582388292c87871245688c6d00bee05
shitao <133397418@qq.com>
2024-01-14 14:39:18 +0800

    ✨ 功能(docker_start.sh)：增加脚本中配置和计划任务文件存在性检查 ⬆️ 升级(docker_start.sh)：更新默认下载文件链接，使用jsDelivr CDN加速 🔧 配置(docker_start.sh)：将硬编码的文件路径替换为变量存储路径

--- docker_start.sh
+++ docker_start.sh
@@ -4,11 +4,22 @@ echo "在当前目录下创建 config 和 cron 文件夹"
 mkdir -p config
 mkdir -p cron
 
-echo "下载 config 文件"
-curl https://raw.githubusercontent.com/sitoi/dailycheckin/main/docker/config.template.json -o config/config.json
+CONFIG_FILE="config/config.json"
+CRONTAB_FILE="cron/crontab_list.sh"
 
-echo "下载 crontab_list.sh 文件"
-curl https://raw.githubusercontent.com/sitoi/dailycheckin/main/docker/crontab_list.sh -o cron/crontab_list.sh
+if [ ! -f "$CONFIG_FILE" ]; then
+    echo "config.json 不存在. 开始下载默认文件..."
+    curl https://cdn.jsdelivr.net/gh/sitoi/dailycheckin@main/docker/config.template.json -o $CONFIG_FILE
+else
+    echo "config.json 已存在. 跳过下载。"
+fi
+
+if [ ! -f "$CRONTAB_FILE" ]; then
+    echo "crontab_list.sh 不存在. 开始下载默认文件..."
+    curl https://cdn.jsdelivr.net/gh/sitoi/dailycheckin@main/docker/crontab_list.sh -o $CRONTAB_FILE
+else
+    echo "crontab_list.sh 已存在. 跳过下载。"
+fi
 
 
 docker --version
@@ -30,7 +41,7 @@ else
   else
     echo "docker-compose 环境存在，将使用 docker-compose 命令启动容器..."
     echo "下载 docker-compose.yml 文件"
-    curl -O https://raw.githubusercontent.com/sitoi/dailycheckin/main/docker/docker-compose.yml
+    curl -O https://cdn.jsdelivr.net/gh/sitoi/dailycheckin@main/docker/docker-compose.yml
     echo "开始通过 docker-compose 命令创建容器"
     docker-compose pull
     docker-compose up -d
